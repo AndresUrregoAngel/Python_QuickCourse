84a202a2049f65365b14fffe595b1f05b17496fa
Owen <owenickoazul@gmail.com>
2018-02-10 11:30:23 -0500

    Solucion 3
    Descripcion del codigo para leer la cantidad de filas

--- Practical/Solutions/Jan1318.py
+++ Practical/Solutions/Jan1318.py
@@ -1,13 +1,47 @@
-input_f = open("E:\\Sources\\ssa-pop3-eng.csv", "r")
-output_f = open("E:\\Sources\\output.txt", "w")
-next(input_f)
-
+input_f = open("C:\\Users\\Hewlett Packard\Documents\\Ejercicios Python\\pensionados.txt","r")
+output_f = open('C:\\Users\\Hewlett Packard\Documents\\Ejercicios Python\\NewFile.txt',"w")
+#next(input_f)
+'''
 for i in input_f:
     x = i.split(',')
     if (int(x[3])>1000):
         new_line = "{},{},{}".format(str(x[0]),str(x[1]),str(x[3]))
         output_f.write(new_line)
 
+#next(input_f)
+counter= 0
+New_list=[]
+for i in input_f:
+    x= i.split(',')
+    Date= x[0]
+    año= Date[:4]
+    output_f.write(año)
+    #print(año)
+    if (x[1] == 'Veterans Affairs Canada' and int(año) == 2010):
+        #print(año, x[1], x[3])
+        New_list.append(int(x[3]))
+
+
+suma= 0
+for i in New_list:
+    suma= suma + i
+    counter= counter + 1
+
+
+
+print("El promedio de los pensionados en Veterans Affairs Canada es: ", round(suma/counter,1))
+'''
+counter= 0
+for i in input_f:
+    x= i.split(',')
+    counter= counter + 1
+    #len(i + str(counter))
+
+
+print("The number of rows that are in the files are:", str(counter))
+
+for i in  input_f:
+    x= i.split(',')
 
 
 
@@ -17,6 +51,12 @@ for i in input_f:
 
 
 
-for element in input_f:
-    print(element)
+
+
+
+
+
+
+
+
 
---
+++ Practical/Solutions/Titanic.py
@@ -0,0 +1,28 @@
+#with open("C:\\Users\\Hewlett Packard\\Documents\\Ejercicios Python\\titanic1.csv", 'r') as f:
+    #print (f.read())
+
+#new_file= open("C:\\Users\\Hewlett Packard\Documents\\Ejercicios Python\\titanic_2.txt", 'w')
+'''
+next(titanic)
+sex= []
+counter= 0
+for e in titanic:
+    x= e.split(',')
+    if (x[2]== "Female" and "Masculine"):
+        sex.append(int(x[4]))
+        print(sex)
+'''
+f= open("C:\\Users\\Hewlett Packard\\Documents\\Ejercicios Python\\titanic1.csv", 'r')
+c= open("C:\\Users\\Hewlett Packard\Documents\\Ejercicios Python\\titanic_2.txt", 'w')
+new= []
+counter= 0
+male= 0
+for i in f:
+    x= i.split(',')
+    if (x[5] == str ("male")):
+        male= male + 1
+        print(male)
+        #new.append(x[5])
+    else:
+        
+
