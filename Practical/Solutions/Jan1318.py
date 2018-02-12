9e070487d60d1fb6617cfbee3f1b9f72e499e898
Andres Urrego <ingenieroandresangel@gmail.com>
2018-02-12 10:37:00 -0500

    Adding bonus point
    take a look the bonus point with a method deployment

--- Practical/Solutions/Jan1318.py
+++ Practical/Solutions/Jan1318.py
@@ -40,15 +40,42 @@ for i in input_f:
 
 print("The number of rows that are in the files are:", str(counter))
 
-for i in  input_f:
-    x= i.split(',')
+## Bonus point to get the rows qty per year using a custumized method
+
+input_f = open("C:\\Users\\P928260\\Downloads\\ssa-pop3-eng.csv","r")
+next(input_f)
+
+years_unique = []
+
+for line in input_f:
+    item = line.split(',')
+    year_f = item[0][:4]
+    if (year_f not in years_unique):
+        years_unique.append(year_f)
 
 
+input_f.close()
 
 
-
-
-
+def calculaterows(years,file):
+    for year in years_unique:
+            input_f = open(file,"r")
+            next(input_f)
+            counter_rows = 0
+            
+            for line in input_f:
+                item = line.split(',')
+                year_f = item[0][:4]
+                if (year == year_f):
+                    counter_rows += 1
+                    
+            input_f.close()
+            
+            print("the year {} has {} of rows".format(year,counter_rows))
+    
+    
+path = "C:\\Users\\P928260\\Downloads\\ssa-pop3-eng.csv"
+calculaterows(years_unique,path)   
 
 
 
