caabf91aa19b45486da3c83fcbf39356f39b0eb9
Andres Urrego <ingenieroandresangel@gmail.com>
2018-03-17 18:36:54 -0700

    New version

--- Practical/Solutions/02.py
+++ Practical/Solutions/02.py
@@ -27,6 +27,8 @@ def conteo_smaterter (Plist):
         #counter_list.append(counter)
 
     return #counter_list
+
+
 def conteo_filas (fila):
     counter= 0
     for e in new_list:
@@ -41,6 +43,8 @@ def suma_lista (l): # Es un metodo con el fin de hacer una suma
         suma = suma + e
 
     return suma
+
+
 def suma_smart (List):
     any_list: []
     for year in List:
@@ -59,6 +63,16 @@ def suma_smart (List):
 
 
 
+def compare_lists(list1,list2):
+
+
+    for item1 in list1:
+        total_sum = 0
+        for item2 in list2:
+            if (item1 == item2[0]):
+                total_sum = total_sum + float(item2[1])
+
+        print("the territory:", item1 ," has penalty amount of:", total_sum)
 
 
 
@@ -71,32 +85,31 @@ def suma_smart (List):
 
 
 #########Solutions##########
-file= open("C:\\Users\\Todos\\Documents\\Owen'sExercises\\Curso\\Python_QuickCourse\\Multas_y_Sanciones_SECOP_I.csv", 'r', encoding='cp437')
+file= open("E:\\Sources\\Multas_y_Sanciones_COL\\Multas_y_Sanciones_SECOP_I.csv", 'r', encoding='cp437')
 territory= []
 new_list= []
 zona= []
 multas_transmilenio= []
 multas_bosa= []
 year=[]
+penalty_terrlist = []
 
 
 next(file)
 for i in file:
     z= i.split(",")
     #print(z[0])
+    territory.append(z[3])
+    new_list.append(z[3])
     if ("TRANSMILENIO" in z[0]):
         multas_transmilenio.append (int(z[9]))
     if ("ALCALDAìA LOCAL DE BOSA" in z[0]):
         multas_bosa.append(int(z[9]))
-
-#for i in multas_bosa:
-  #  print(i)
-
-
+    penalty_terr = z[3],z[9]
+    penalty_terrlist.append(penalty_terr)
 
 """
-    territory.append(z[3])
-    new_list.append(z[3])
+   
     zona.append(z[0])
     multa.append(z[9])
 
@@ -104,27 +117,16 @@ for i in file:
 print(multa)
 """
 '''
-print('En el territorio Distrito Capital hay:',conteo(territory,'distrito capital'))
-print("En el territorio Nacional hay:", conteo(territory,'nacional'))
-print("En el Territorio Nacional Centralizado hay:", conteo(territory,'nacional centralizado'))
-print("En el Territorio Departamental Centralizado hay:", conteo(territory,'territorial departamental centralizado'))
-print("En el Territorio Departamental Descentralizado hay:", conteo(territory,'territorial departamental descentralizado'))
-print("En el Territorio Distrital Municipal Nivel 1 hay:", conteo(territory,'territorial distrital municipal nivel 1'))
-print("En el Territorio Distrital Municipal Nivel 2 hay:", conteo(territory,'territorial distrital municipal nivel 2'))
-print("En el Territorio Distrital Municipal Nivel 3 hay:", conteo(territory,'territorial distrital municipal nivel 3'))
-print("En el Territorio Distrital Municipal Nivel 4 hay:", conteo(territory,'territorial distrital municipal nivel 4'))
-print("En el Territorio Distrital Municipal Nivel 5 hay:", conteo(territory,'territorial distrital municipal nivel 5'))
-print("En el Territorio Distrital Municipal Nivel 6 hay:", conteo(territory,'territorial distrital municipal nivel 6'))
-
 conteo_smaterter(territory)
 
 for e in file:
     z= i.split(",")
     new_list.append(z[3])
 '''
-#conteo_smaterter(territory)
-#print("El numero de filas es:", conteo_filas(new_list))
-#print("Hya:", suma(zona,'BOGOTAü D.C. - TRANSMILENIO', multa))
-print("Transmilnio pago una multa de: ", suma_lista(multas_transmilenio))
-print("Bosa pago una multa de: ", suma_lista(multas_bosa))
-suma_smart(year)
+
+#print(conteo_smaterter(territory)) # Point 1
+
+#print("El numero de filas es:", conteo_filas(new_list)) # Point 3
+#print("Transmilenio pago una multa de: ", suma_lista(multas_transmilenio)) # Point 4
+#print("Bosa pago una multa de: ", suma_lista(multas_bosa)) # Point 5
+print(compare_lists(territory,penalty_terrlist))
