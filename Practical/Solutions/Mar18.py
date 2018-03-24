241a0f8c2ea87eedc32383b0314582385d91c4c0
Owen <owenickoazul@gmail.com>
2018-03-23 19:25:22 -0500

    #3

--- Practical/Solutions/Mar18.py
+++ Practical/Solutions/Mar18.py
@@ -8,6 +8,38 @@ def conteo_filas (fila):
     return counter
 
 
+def conteo_smaterter (Plist):
+    item_unique = []
+
+    for item in Plist:
+        if (item not in item_unique):
+            item_unique.append(item)
+
+
+    for item in item_unique:
+
+        counter = 0
+        for line in Plist:
+            if(line == item):
+                counter = counter + 1
+        print("- language of Programing: ",item , "were surveyed:",counter)
+
+    return
+
+def average_rows (any_list,name,list_cost):
+    counter= 0
+    sum= 0
+    average = 0
+    for e in any_list:
+
+        for i in list_cost:
+        if (e == name ):
+            counter = counter + 1
+            for i in list_cost:
+                sum = sum + int(i)
+                average = sum / counter
+             # print(sum)
+    return counter, average, sum
 
 
 
@@ -15,15 +47,35 @@ def conteo_filas (fila):
 
 
 
-########### Solotions #####
-file= open('E:\\Sources\\Salaries\\rawdata.csv', "r") #open('C:\\Users\\Hewlett Packard\\Documents\\Documentos Python\\Salaries\\rawdata.csv', "r")
+
+
+
+
+
+
+########### Solotions ################
+file= open ('C:\\Users\\Hewlett Packard\\Documents\\Documentos Python\\Salaries\\Salaries\\rawdata.csv', "r")
 new_file= []
+lengu_file= []
+cost_list= []
 for e in file:
    z= e.split(",")
    new_file.append(z)
-   print(z[1])
+   lengu_file.append(z[11])
+   cost_list.append(z[19])
+#print(cost_list)
+
+#print(lengu_file)
 
 ## 1
-print(conteo_filas(new_file))
+#print(conteo_filas(new_file))
 
 ## 2
+#print(conteo_smaterter(lengu_file))
+
+## 3
+print(average_rows(lengu_file,"Python", cost_list))
+print(cost_list)
+## 4
+
+for e in cost_list
